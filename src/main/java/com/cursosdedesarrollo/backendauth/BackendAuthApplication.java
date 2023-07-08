65b6e8b04a0e3006103cd1875579633358b6a17e
David Vaquero <pepesan@gmail.com>
2023-07-08 17:34:07 +0200

    Initial commit

---
+++ src/main/java/com/cursosdedesarrollo/backendauth/BackendAuthApplication.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.backendauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursosdedesarrollo/backendauth/BackendAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.backendauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
