87b167cd9125b481ffc9cdf5dfb8aca95a6531e8
David Vaquero <pepesan@gmail.com>
2023-07-22 19:48:11 +0200

    metida config de test y ficheros iniciales de schema y datos pero sin cargarlos de momento en el arranque

--- src/main/java/com/cursosdedesarrollo/backendauth/domain/User.java
+++ src/main/java/com/cursosdedesarrollo/backendauth/domain/User.java
@@ -16,7 +16,7 @@ import java.util.Set;
 @Table(name = "AuthUsers")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
